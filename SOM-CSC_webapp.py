bac83dd44dd278cf4e2673b6838a334b13ade6ba
Andrea Scaudo <scaudo.andrea@gmail.com>
2024-02-08 09:48:07 -0500

    bug fixes

--- SOM-CSC_webapp.py
+++ SOM-CSC_webapp.py
@@ -114,109 +114,114 @@ else:
 
 
 with st.form(key='plot_form'):
-    st.write('## Select which plot to display')
-    plot_type = st.selectbox(
-        'Plot type', ['U-Matrix', 'Scatter Visualization [cluster]', 'Category Visualization [cluster]', 'Feature Visualization', 'Custom Feature Visualization'], help='Select the type of visualization to display')
-    plot_submit = st.form_submit_button('Show plot')
+    if st.session_state.SOM_loaded:
+        st.write('## Select which plot to display')
+        plot_type = st.selectbox(
+            'Plot type', ['U-Matrix', 'Scatter Visualization [cluster]', 'Category Visualization [cluster]', 'Feature Visualization', 'Custom Feature Visualization'], help='Select the type of visualization to display')
+        plot_submit = st.form_submit_button('Show plot')
 
-    if plot_submit:
-        if plot_type == 'U-Matrix':
-            # U-Matrix
-            st.write('## U-Matrix')
-            with st.expander("See explanation"):
-                st.write(
-                    'The U-Matrix is a visualization tool that represents the distance between neurons in the SOM. It is a 2D grid where each cell is colored based on the distance between the neurons. The U-Matrix is a useful tool to visualize the topology of the SOM and identify clusters of neurons.')
-            if st.session_state.som.topology == 'rectangular':
-                plot_rectangular_u_matrix(st.session_state.som)
-            else:
-                plot_u_matrix_hex(st.session_state.som)
-                # test()
-        elif plot_type == 'Scatter Visualization [cluster]':
-            # Scatter plot
-            if st.session_state.som.topology == 'rectangular':
-                scatter_plot_clustering(
+        if plot_submit:
+            if plot_type == 'U-Matrix':
+                # U-Matrix
+                st.write('## U-Matrix')
+                with st.expander("See explanation"):
+                    st.write(
+                        'The U-Matrix is a visualization tool that represents the distance between neurons in the SOM. It is a 2D grid where each cell is colored based on the distance between the neurons. The U-Matrix is a useful tool to visualize the topology of the SOM and identify clusters of neurons.')
+                if st.session_state.som.topology == 'rectangular':
+                    plot_rectangular_u_matrix(st.session_state.som)
+                else:
+                    plot_u_matrix_hex(st.session_state.som)
+                    # test()
+            elif plot_type == 'Scatter Visualization [cluster]':
+                # Scatter plot
+                if st.session_state.som.topology == 'rectangular':
+                    scatter_plot_clustering(
+                        st.session_state.som, X, GMM_cluster_labels)
+                else:
+                    scatter_plot_clustering_hex(
+                        st.session_state.som, X, GMM_cluster_labels)
+            elif plot_type == 'Category Visualization [cluster]':
+                # Category plot
+                category_map = project_feature(
                     st.session_state.som, X, GMM_cluster_labels)
-            else:
-                scatter_plot_clustering_hex(
-                    st.session_state.som, X, GMM_cluster_labels)
-        elif plot_type == 'Category Visualization [cluster]':
-            # Category plot
-            category_map = project_feature(
-                st.session_state.som, X, GMM_cluster_labels)
-            if st.session_state.som.topology == 'rectangular':
-                category_plot_clustering(category_map)
-            else:
-                category_plot_clustering_hex(category_map)
-        elif plot_type == 'Feature Visualization':
-            # Feature plot
-            # subform to select a features
-            with st.expander("See explanation"):
-                st.write(
-                    'This visualization will color the pre-trained SOM based on a selected feature, implementing a scaling method to each neuron.')
-                st.write(
-                    'For example, if the selected feature represents the mean value of a particular column, each neuron\'s color will reflect the average of that column.')
-                st.write(
-                    'Additionally, the frequency of samples at a specific neuron indicates the number of times that neuron was identified as the best matching unit for a sample.')
-            feature = st.selectbox(
-                'Feature', st.session_state.raw_df.columns.to_list())
-            scaling = st.selectbox(
-                'Scaling', ['mean', 'min', 'max', 'sum', 'median', 'std'])
-            st.write(
-                "###### To update the map with the selected feature and applied scaling, please click the 'Show Plot' button again.")
-            var = project_feature(
-                st.session_state.som, X, st.session_state.raw_df[feature])
-            if st.session_state.som.topology == 'rectangular':
-                features_plot(var, scaling=scaling)
-            else:
-                features_plot_hex(var, scaling=scaling)
-        elif plot_type == 'Custom Feature Visualization':
-            with st.expander("See explanation"):
-                st.write('This visualization tool enables coloring of the pre-trained SOM based on data from a newly uploaded dataset, allowing users to dynamically select which feature to use for coloring. Users have the flexibility to upload a new dataset and choose a specific feature that will be applied to color the previously trained SOM.')
-                st.write(
-                    '⚠️ **The uploaded file must to be a .csv file with a specific structure:** ⚠️')
-                st.write(
-                    'The initial columns should contain the features utilized for training the SOM, and these features must be already normalized. The final column should represent the feature intended for coloring the map. For instance, if the SOM was trained with features A, B, and C (all normalized), and you wish to color the map using feature D, the CSV file should be organized as follows:')
-                st.write(
-                    '**A, B, C, D**')
-                st.write(
-                    '*x1, y1, z1, 1*')
-                st.write(
-                    '*x2, y2, z2, 2*')
-                st.write(
-                    '*...*')
-                st.write(
-                    '*xn, yn, zn, n*')
-            st.write(
-                "###### To display the map with the loaded dataset and applied scaling, please click the 'Show Plot' button again.")
-
-            if st.session_state.SOM_loaded:
-                uploaded_file = st.file_uploader(
-                    "Upload your CSV file", type="csv")
+                if st.session_state.som.topology == 'rectangular':
+                    category_plot_clustering(category_map)
+                else:
+                    category_plot_clustering_hex(category_map)
+            elif plot_type == 'Feature Visualization':
+                # Feature plot
+                # subform to select a features
+                with st.expander("See explanation"):
+                    st.write(
+                        'This visualization will color the pre-trained SOM based on a selected feature, implementing a scaling method to each neuron.')
+                    st.write(
+                        'For example, if the selected feature represents the mean value of a particular column, each neuron\'s color will reflect the average of that column.')
+                    st.write(
+                        'Additionally, the frequency of samples at a specific neuron indicates the number of times that neuron was identified as the best matching unit for a sample.')
+                feature = st.selectbox(
+                    'Feature', st.session_state.raw_df.columns.to_list())
                 scaling = st.selectbox(
                     'Scaling', ['mean', 'min', 'max', 'sum', 'median', 'std'])
-                if uploaded_file is not None:
-                    # Attempt to load and validate the dataset
-                    try:
-                        # Assuming a new function 'validate_and_load_dataset' in som_fun.py
-                        dataset = validate_and_load_dataset(
-                            uploaded_file, features)
-                        # All rows, all columns except the last
-                        Xx = dataset.iloc[:, :-1].to_numpy()
-                        # All rows, only the last column
-                        feature = dataset.iloc[:, -1]
+                st.write(
+                    "###### To update the map with the selected feature and applied scaling, please click the 'Show Plot' button again.")
+                var = project_feature(
+                    st.session_state.som, X, st.session_state.raw_df[feature])
+                if st.session_state.som.topology == 'rectangular':
+                    features_plot(var, scaling=scaling)
+                else:
+                    features_plot_hex(var, scaling=scaling)
+            elif plot_type == 'Custom Feature Visualization':
+                with st.expander("See explanation"):
+                    st.write('This visualization tool enables coloring of the pre-trained SOM based on data from a newly uploaded dataset, allowing users to dynamically select which feature to use for coloring. Users have the flexibility to upload a new dataset and choose a specific feature that will be applied to color the previously trained SOM.')
+                    st.write(
+                        '⚠️ **The uploaded file must to be a .csv file with a specific structure:** ⚠️')
+                    st.write(
+                        'The initial columns should contain the features utilized for training the SOM, and these features must be already normalized. The final column should represent the feature intended for coloring the map. For instance, if the SOM was trained with features A, B, and C (all normalized), and you wish to color the map using feature D, the CSV file should be organized as follows:')
+                    st.write(
+                        '**A, B, C, D**')
+                    st.write(
+                        '*x1, y1, z1, 1*')
+                    st.write(
+                        '*x2, y2, z2, 2*')
+                    st.write(
+                        '*...*')
+                    st.write(
+                        '*xn, yn, zn, n*')
+                st.write(
+                    "###### To display the map with the loaded dataset and applied scaling, please click the 'Show Plot' button again.")
 
-                        if dataset is not None:
-                            st.session_state['new_dataset'] = dataset
-                            # Call to project_feature and features_plot goes here, using the new dataset
-                            var = project_feature(
-                                st.session_state.som, Xx, feature)
-                            st.write("DEBUG")
-                            if st.session_state.som.topology == 'rectangular':
-                                features_plot(var, scaling=scaling)
+                if st.session_state.SOM_loaded:
+                    uploaded_file = st.file_uploader(
+                        "Upload your CSV file", type="csv")
+                    scaling = st.selectbox(
+                        'Scaling', ['mean', 'min', 'max', 'sum', 'median', 'std'])
+                    if uploaded_file is not None:
+                        # Attempt to load and validate the dataset
+                        try:
+                            # Assuming a new function 'validate_and_load_dataset' in som_fun.py
+                            dataset = validate_and_load_dataset(
+                                uploaded_file, features)
+                            # All rows, all columns except the last
+                            Xx = dataset.iloc[:, :-1].to_numpy()
+                            # All rows, only the last column
+                            feature = dataset.iloc[:, -1]
+
+                            if dataset is not None:
+                                st.session_state['new_dataset'] = dataset
+                                # Call to project_feature and features_plot goes here, using the new dataset
+                                var = project_feature(
+                                    st.session_state.som, Xx, feature)
+                                st.write("DEBUG")
+                                if st.session_state.som.topology == 'rectangular':
+                                    features_plot(var, scaling=scaling)
+                                else:
+                                    features_plot_hex(var, scaling=scaling)
                             else:
-                                features_plot_hex(var, scaling=scaling)
-                        else:
-                            st.error(
-                                "Dataset validation failed. Please check the column names and ensure there are no empty values.")
-                    except Exception as e:
-                        st.error(f"An error occurred: {e}")
+                                st.error(
+                                    "Dataset validation failed. Please check the column names and ensure there are no empty values.")
+                        except Exception as e:
+                            st.error(f"An error occurred: {e}")
+    else:
+        st.write('## SOM model not loaded')
+        st.write(
+            'Please load a SOM model or generate a new one to visualize the map.')
